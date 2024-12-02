009c0a551b6a9b550418bb77517a180e3ba60a14
April Hall <ari@arithefirst.com>
2024-12-01 23:41:28 -0500

    Adjust title and desc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,8 +10,6 @@
 </script>
 
 <svelte:head>
-  <title>Pronoun Dressing Room</title>
-  <meta name="Description" content="A place to try on pronouns and names to find the ones that work" />
   {#if darkMode}
     <link rel="icon" href="/wardrobe-dark.svg" />
   {:else}
