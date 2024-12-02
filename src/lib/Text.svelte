29cb7179f7cf8157f060cdadc246981822174b6d
April Hall <ari@arithefirst.com>
2024-12-01 22:14:11 -0500

    Update link colors

--- src/lib/Text.svelte
+++ src/lib/Text.svelte
@@ -16,7 +16,9 @@
       been wonderful. I hope so!
     </p>
     <p class="text-xs px-4">
-      Authored by <a class="underline" href="https://underneathbubbles.tumblr.com/">underneathbubbles</a>
+      Authored by <a class="underline text-primary-500" href="https://underneathbubbles.tumblr.com/"
+        >underneathbubbles</a
+      >
     </p>
 
     <p class="pt-6 px-4">
@@ -25,22 +27,26 @@
       {#if dataObj.plural}were{:else}was{/if} a child. {dataObj.name} is sooo talented, I wish I was {dataObj.obj}.
     </p>
     <p class="text-xs px-4">
-      Authored by <a class="underline" href="https://reddit.com/u/AlexandraLikesCake">u/AlexandraLikesCake</a>
+      Authored by <a class="underline text-primary-500" href="https://reddit.com/u/AlexandraLikesCake"
+        >u/AlexandraLikesCake</a
+      >
     </p>
 
     <p class="pt-6 px-4">
       Did you see that one {dataObj.top}'s new haircut? I think {dataObj.possDeter} name is {dataObj.name}. It's really
       impressive considering {dataObj.subj} cut it {dataObj.refl} using {dataObj.possDeter} scissors.
     </p>
-    <p class="text-xs px-4">Authored by <a class="underline" href="https://www.arithefirst.com">arithefirst</a></p>
+    <p class="text-xs px-4">
+      Authored by <a class="underline text-primary-500" href="https://www.arithefirst.com">arithefirst</a>
+    </p>
 
     <p class="pt-6 px-4">
       Hey, it's {dataObj.name}! {dataObj.subj}'{#if dataObj.plural}re{:else}s{/if} so cute and smart! {dataObj.name} is really
       good with computers too, {dataObj.subj} helped me out recently. I hope I'll be meeting {dataObj.obj} again soon, I
       love to hang out with {dataObj.obj}!
     </p>
-    <p class="text-xs px-4">
-      Authored by <a class="underline" href="https://reddit.com/u/Dojo-Tekina">u/Dojo-Tekina</a>
+    <p class="text-xs px-4 pb-4">
+      Authored by <a class="underline text-primary-500" href="https://reddit.com/u/Dojo-Tekina">u/Dojo-Tekina</a>
     </p>
   </div>
 </div>
