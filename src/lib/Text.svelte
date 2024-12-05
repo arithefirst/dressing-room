33f29ebf169a7681055da4a6089686ca01af3d7a
April Hall <ari@arithefirst.com>
2024-12-05 14:18:06 -0500

    Prettier format

--- src/lib/Text.svelte
+++ src/lib/Text.svelte
@@ -2,7 +2,7 @@
   import Paragraph1 from '$lib/paragraphs/Paragraph1.svelte';
   import Paragraph2 from '$lib/paragraphs/Paragraph2.svelte';
   import Paragraph3 from '$lib/paragraphs/Paragraph3.svelte';
-  import Paragraph4 from '$lib/paragraphs/Paragraph4.svelte'
+  import Paragraph4 from '$lib/paragraphs/Paragraph4.svelte';
 </script>
 
 <div class="w-full h-fit p-4 mb-4">
